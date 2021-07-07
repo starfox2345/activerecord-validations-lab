bfe408cb07a1a58eac25a048c8a1af6be727b70f
yatsun <yatsunlam25@gmail.com>
2021-07-07 12:17:32 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,35 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    CBAIT = [
+        /Won't Believe/,
+        /Secret/,
+        /Top [0..9]/,
+        /Guess/
+    ]
+
+    def clickbait?
+        if CBAIT.none? { |bait| bait.match title }
+            errors.add(:title, "clicbait")
+        end
+    end
+
+
+    # CUSTOM = [
+    #     "Won't Believe",
+    #     "Secret",
+    #     "Top[number]"
+    #     "Guess"
+    # ]
+    
+    # def clickbait?
+    #      if title.present? && CUSTOM.none? { |p| title.match(p)}
+    #         errors.add(:title, "clickbait-y... enough?")
+    #     end
+    # end
+
 end
