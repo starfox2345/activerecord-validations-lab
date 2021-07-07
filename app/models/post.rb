b98dc50baab3b8715308c7b39d2acf344f480745
yatsun <yatsunlam25@gmail.com>
2021-07-07 19:04:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,31 +5,40 @@ class Post < ActiveRecord::Base
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
     validate :clickbait?
 
-    CBAIT = [
-        /Won't Believe/,
-        /Secret/,
-        /Top [0..9]/,
-        /Guess/
-    ]
 
-    def clickbait?
-        if CBAIT.none? { |bait| bait.match title }
-            errors.add(:title, "clicbait")
-        end
-    end
-
-
-    # CUSTOM = [
-    #     "Won't Believe",
-    #     "Secret",
-    #     "Top[number]"
-    #     "Guess"
-    # ]
-    
     # def clickbait?
-    #      if title.present? && CUSTOM.none? { |p| title.match(p)}
-    #         errors.add(:title, "clickbait-y... enough?")
+    #     if self.name.include?("Won't Believe", "Secret", "Top[number]", "Guess")
+        
+    #     else
+    #         "clickbait-y?"
     #     end
     # end
+
+    # CBAIT = [
+    #     /Won't Believe/,
+    #     /Secret/,
+    #     /Top [0..9]/,
+    #     /Guess/
+    # ]
+
+    # def clickbait?
+    #     if CBAIT.none? { |bait| bait.match title }
+    #         errors.add(:title, "clicbait")
+    #     end
+    # end
+
+
+    CUSTOM = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+    
+    def clickbait?
+         if title.present? && CUSTOM.none? { |p| title.match(p)}
+            errors.add(:title, "clickbait-y... enough?")
+        end
+    end
 
 end
